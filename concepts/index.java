6b5a0d1644ff19c1a594fb298a69922da0c3c98f
Sundaram Kumar Jha <jhasundarm@gmail.com>
2023-02-22 22:45:52 +0530

    practising basics

---
+++ concepts/index.java
@@ -0,0 +1,88 @@
+package concepts;
+
+// import java.util.Arrays;
+import java.util.Scanner;
+
+class Hello {
+    public static void main(String a[]) {
+        try (Scanner input = new Scanner(System.in)) {
+            // System.out.println("Enter two numbers : ");
+
+            // int num1 = input.nextInt();
+            // int num2 = input.nextInt();
+
+            // System.out.println("Enter the arithmatic operation you want to perform");
+            // char op = input.next().charAt(0);
+
+            // int res = 0;
+
+
+            // if else condition
+
+            // if (op == '+') {
+            // res = num1 + num2;
+            // System.out.println("Addition : " + res);
+            // } else if (op == '-') {
+            // res = num1 - num2;
+            // System.out.println("Subtraction : " + res);
+            // } else if (op == '*') {
+            // res = num1 * num2;
+            // System.out.println(res);
+            // } else if (op == '/') {
+            // res = num1 / num2;
+            // System.out.println(res);
+            // } else {
+            // System.out.println("Please enter correct operator");
+            // }
+
+            // switch cases
+
+            // switch (op) {
+            //     case '+':
+            //         res = num1 + num2;
+            //         break;
+            //     case '-':
+            //         res = num1 - num2;
+            //         break;
+            //     case '*':
+            //         res = num1 * num2;
+            //         break;
+            //     case '/':
+            //         res = num1 / num2;
+            //         break;
+            //     default:
+            //         System.out.println("Please enter correct operator");
+            // }
+
+            // System.out.println(op + " of "+ num1 +" and "+ num2 + " = "+res);
+
+
+            /// ARRAYs -----------------------
+
+            int arr[] = { 23, 134, 156, 67, 678 };
+
+            // int res =  arr[0];
+
+            // Q).  finding the greatest number in array
+
+            // FOR LOOPS ----
+
+            // for(int i=0;i<arr.length;i++){
+            //     if(res < arr[i])
+            //         res = arr[i];
+            // }
+
+            // System.out.println("Greatest number  = "+ res);
+
+            // Q). finding the smallest number 
+
+            /// for each loop
+            // for(int ele : arr){
+            //     if(ele < res)
+            //        res = ele;
+            // }
+            
+            // System.out.println("smallest number is = "  + res);
+        }
+    }
+}
